31e1ca9c924e8b9099d8c6e3ca6ce6ff15ef2e8c
gooanq <ivanguzikxv@gmail.com>
2023-03-27 18:24:24 -0400

    Add test project

---
+++ ShopApi.Test/Authentication/ValidatorTest.cs
@@ -0,0 +1,24 @@
+﻿using ShopApi.Authentication;
+
+namespace ShopApi.Test.Authentication
+{
+	public class ValidatorTest
+	{
+		private readonly Validator _validator;
+
+		public ValidatorTest()
+		{
+			_validator = new Validator();
+		}
+
+
+		[Theory]
+		[InlineData("ivan.huzikov@gmail.com")]
+		[InlineData("joker777@yahoo.com")]
+		[InlineData("batman@test.ca")]
+		public void VaidateEmail_EmaiIsValid_ReturnsTrue(string email)
+		{
+			Assert.True(_validator.ValidateEmail(email));
+		}
+	}
+}
---
+++ ShopApi.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ShopApi.Test
+{
+	public class UnitTest1
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
